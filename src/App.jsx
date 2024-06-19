c1e2079596ba10a7fbed6c1d323fa6012dfcdbbd
SpectralGT <atharvsingh1396@gmail.com>
2024-06-19 14:18:33 +0530

    created header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Banner from "./components/Banner.jsx";
+import Header from "./components/Header.jsx";
 import LandingPage from "./panels/LandingPage.jsx";
 function App() {
   return (
     <>
       <Banner text="#Opent to Work"></Banner>
+      <Header></Header>
       <LandingPage></LandingPage>
     </>
   );
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import "./Header.css";
+
+export default function Header() {
+  return (
+    <div className="header">
+      <p>AS</p>
+      <p>My Work</p>
+      <p>Skills</p>
+      <p>Blog</p>
+    </div>
+  );
+}
