3ad668f0e907c740b874e390443ab3319b6b0ec6
SpectralGT <atharvsingh1396@gmail.com>
2024-06-21 16:13:24 +0530

    fixed responsiveness

--- src/panels/LandingPage.jsx
+++ src/panels/LandingPage.jsx
@@ -4,7 +4,10 @@ export default function LandingPage() {
   return (
     <div id="LandingPage">
       <p>HI IM</p>
-      <p>ATHARV SINGH</p>
+      <p>ATHARV </p>
+      <p>SINGH</p>
+
+      <p className="description">I am a Passionate Game dev and Web developer</p>
     </div>
   );
 }
