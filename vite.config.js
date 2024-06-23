ac72668e7cdfefa9d5a5f892bfcb70dba78d8799
SpectralGT <atharvsingh1396@gmail.com>
2024-06-23 20:49:34 +0530

    commit

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/spectralgt.github.io",
   plugins: [react()],
   resolve: {
     modules: ["node_modules"],
