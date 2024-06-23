9139dcd3c68eb6701634806e7f5907d6ca2b377e
SpectralGT <atharvsingh1396@gmail.com>
2024-06-23 17:55:36 +0530

    fixed contact page

--- src/panels/ContactMe.jsx
+++ src/panels/ContactMe.jsx
@@ -5,13 +5,45 @@ export default function ContactMe() {
   return (
     <>
       <StickyBanner text="Contact Me"></StickyBanner>
-      <p className="heading">ATHARV SINGH</p>
       <div className="contact">
-        <p>atharvsingh1396@gmail.com</p>
-        <p>LinkedIn</p>
-        <p>GitHub</p>
-        <p>PlayStore</p>
-        <p>Itch.io</p>
+        <div className="links">
+          <p>
+            <i className="fa-solid fa-envelope"></i> atharvsingh1396@gmail.com
+          </p>
+          <p>
+            <i className="fa-brands fa-linkedin"></i> LinkedIn
+          </p>
+          <p>
+            <i className="fa-brands fa-github"></i> GitHub
+          </p>
+          <p>
+            <i className="fa-brands fa-google-play"></i> PlayStore
+          </p>
+          <p>
+            <i className="fa-brands fa-itch-io"></i> Itch.io
+          </p>
+        </div>
+        <div>
+          <p className="heading">ATHARV</p>
+          <p className="heading">SINGH</p>
+        </div>
+        <div className="links2">
+          <p>
+            atharvsingh1396@gmail.com <i className="fa-solid fa-envelope"></i>
+          </p>
+          <p>
+            LinkedIn <i className="fa-brands fa-linkedin"></i>
+          </p>
+          <p>
+            GitHub <i className="fa-brands fa-github"></i>
+          </p>
+          <p>
+            PlayStore <i className="fa-brands fa-google-play"></i>
+          </p>
+          <p>
+            Itch.io <i className="fa-brands fa-itch-io"></i>
+          </p>
+        </div>
       </div>
     </>
   );
--- src/panels/MySkills.jsx
+++ src/panels/MySkills.jsx
@@ -4,13 +4,42 @@ export default function MySkills() {
     <div className="skills">
       <p className="heading">MY SKILLS</p>
       <div className="container">
-        <p>HTML</p>
-        <p>CSS</p>
-        <p>JAVASCRIPT</p>
-        <p>UNITY</p>
-        <p>C#</p>
-        <p>PYTHON</p>
+        <p>
+          <i className="fa-brands fa-html5"></i> HTML
+        </p>
+        <p>
+          <i className="fa-brands fa-css3-alt"></i> CSS
+        </p>
+        <p>
+          <i className="fa-brands fa-js"></i> JAVASCRIPT
+        </p>
+        <p>
+          <i className="fa-brands fa-unity"></i> UNITY
+        </p>
+        <p>C #</p>
+        <p>
+          <i className="fa-brands fa-python"></i> PYTHON
+        </p>
         <p>C++</p>
+        <p>REACT</p>
+        <p>THREE.JS</p>
+        <p>VITE</p>
+        <p>SVELTE</p>
+        <p>SOCKET.IO</p>
+        <p>SASS</p>
+        <p>EXPRESS.JS</p>
+        <p>.NET</p>
+        <p>JWT</p>
+        <p>MONGODB</p>
+        <p>MYSQL</p>
+        <p>GIT</p>
+        <p>GITHUB</p>
+        <p>NPM</p>
+        <p>FIREBASE</p>
+        <p>GITHUB PAGES</p>
+        <p>VERCEL</p>
+        <p>TYPESCRIPR</p>
+        <p>GRAPHQL</p>
       </div>
     </div>
   );
