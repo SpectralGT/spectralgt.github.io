199e376ff167f449240af470912c62e61ceef63c
SpectralGT <atharvsingh1396@gmail.com>
2024-06-29 17:17:54 +0530

    changed color and bg

--- src/panels/LandingPage/LandingPage.jsx
+++ src/panels/LandingPage/LandingPage.jsx
@@ -1,31 +1,7 @@
 import "./LandingPage.css";
-// import { useEffect, useRef } from "react";
-
 export default function LandingPage() {
-  // const videoRef = useRef(null);
-  // useEffect(() => {
-  //   videoRef.current.defaultMuted = true;
-  // });
-
   return (
     <div id="landingPage">
-      {/* <div
-        dangerouslySetInnerHTML={{
-          __html: (function () {
-            let videoContainer = document.createElement("video");
-            let source = document.createElement("source");
-            videoContainer.setAttribute("autoplay", "");
-            videoContainer.setAttribute("muted", "");
-            videoContainer.setAttribute("loop", "");
-            source.src = "/bg.mp4";
-            videoContainer.appendChild(source);
-            return videoContainer.outerHTML;
-          })(),
-        }}
-      /> */}
-
-      {/* <video ref={videoRef} muted autoPlay loop src="/bg.mp4" playsInline></video> */}
-
       <video autoPlay loop muted playsInline id="video" src="/bg.mp4" />
       <p>Hi, I'M</p>
       <p>ATHARV </p>
