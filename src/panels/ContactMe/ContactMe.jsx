7e75eb4e77760b8621700313de7670b2853366b1
Atharv Singh <78777556+SpectralGT@users.noreply.github.com>
2025-01-02 14:02:26 +0530

    fiex play store link

--- src/panels/ContactMe/ContactMe.jsx
+++ src/panels/ContactMe/ContactMe.jsx
@@ -18,21 +18,47 @@ export default function ContactMe() {
       <StickyBanner text="Contact Me"></StickyBanner>
       <div className="contact" id="contact">
         <div className="links">
-          <a href="mailto:someone@example.com" target="_blank" rel="noopener noreferrer"><p><i className="fa-solid fa-envelope"></i> &nbsp;  atharvsingh1396@gmail.com</p></a>
-          <a href="https://www.linkedin.com/in/atharv-singh-54bb731a2/" target="_blank" rel="noopener noreferrer"><p><i className="fa-brands fa-linkedin" target="_blank" rel="noopener noreferrer" ></i> &nbsp; LinkedIn</p></a>
-          <a href="https://github.com/SpectralGT" target="_blank" rel="noopener noreferrer" ><p><i className="fa-brands fa-github"></i> &nbsp; GitHub</p></a>
-
+          <a href="mailto:someone@example.com" target="_blank" rel="noopener noreferrer">
+            <p>
+              <i className="fa-solid fa-envelope"></i> &nbsp; atharvsingh1396@gmail.com
+            </p>
+          </a>
+          <a href="https://www.linkedin.com/in/atharv-singh-54bb731a2/" target="_blank" rel="noopener noreferrer">
+            <p>
+              <i className="fa-brands fa-linkedin" target="_blank" rel="noopener noreferrer"></i> &nbsp; LinkedIn
+            </p>
+          </a>
+          <a href="https://github.com/SpectralGT" target="_blank" rel="noopener noreferrer">
+            <p>
+              <i className="fa-brands fa-github"></i> &nbsp; GitHub
+            </p>
+          </a>
         </div>
         <div>
           <p className="heading">ATHARV</p>
           <p className="heading">SINGH</p>
         </div>
         <div className="links2">
-          <a href="https://play.google.com/store/apps/dev?id=5754731511534016621&hl=en" target="_blank" rel="noopener noreferrer"><p>PlayStore &nbsp; <i className="fa-brands fa-google-play"></i></p></a>
-          <a href="https://spectralgt.itch.io/" target="_blank" rel="noopener noreferrer" ><p>Itch.io   &nbsp; <i className="fa-brands fa-itch-io"></i></p></a>
-          <a href="https://atharvsingh.vercel.app/" target="_blank" rel="noopener noreferrer" ><p>Blog      &nbsp; <i className="fa-solid fa-pen"></i></p></a>
-          <a href="https://www.youtube.com/@spectralgt4170" target="_blank" rel="noopener noreferrer" ><p>Youtube   &nbsp; <i className="fa-brands fa-youtube"></i></p></a>
-
+          <a href="https://apps.atharvsingh.co.in/notice" target="_blank" rel="noopener noreferrer">
+            <p>
+              PlayStore &nbsp; <i className="fa-brands fa-google-play"></i>
+            </p>
+          </a>
+          <a href="https://spectralgt.itch.io/" target="_blank" rel="noopener noreferrer">
+            <p>
+              Itch.io &nbsp; <i className="fa-brands fa-itch-io"></i>
+            </p>
+          </a>
+          <a href="https://atharvsingh.vercel.app/" target="_blank" rel="noopener noreferrer">
+            <p>
+              Blog &nbsp; <i className="fa-solid fa-pen"></i>
+            </p>
+          </a>
+          <a href="https://www.youtube.com/@spectralgt4170" target="_blank" rel="noopener noreferrer">
+            <p>
+              Youtube &nbsp; <i className="fa-brands fa-youtube"></i>
+            </p>
+          </a>
         </div>
       </div>
     </>
