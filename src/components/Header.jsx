c0c687401f479968f502d3e181c67afdf9f78b35
Atharv Singh <78777556+SpectralGT@users.noreply.github.com>
2025-01-03 22:01:53 +0530

    changed header link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,17 +9,13 @@ export default function Header() {
       <a href="https://apps.atharvsingh.co.in" target="_blank" rel="noopener noreferrer">
         <p>Apps</p>
       </a>
-      <a href="#skills">
-        <p>Skills</p>
+      <a href="https://spectralgt.itch.io/" target="_blank" rel="noopener noreferrer">
+        <p>Itch.io</p>
       </a>
       <a href="https://blog.atharvsingh.co.in" target="_blank" rel="noopener noreferrer">
         <p>Blog</p>
       </a>
-      <a
-        href="https://drive.google.com/file/d/1QP-ROoDSfAa4sy61mlBS6y2Y0fjYCOFv/view?usp=sharing"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
+      <a href="https://drive.google.com/file/d/1QP-ROoDSfAa4sy61mlBS6y2Y0fjYCOFv/view?usp=sharing" target="_blank" rel="noopener noreferrer">
         <p>Resume</p>
       </a>
       <a href="#contact">
